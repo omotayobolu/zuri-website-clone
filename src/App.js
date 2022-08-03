c07255bc0895a68155e64b411f4a5010a607b8b4
Boluwatife <omotayodaniel10@gmail.com>
2022-08-03 12:14:51 +0100

    desktop view done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "./components/Hero section/Hero";
 import Join from "./components/Join section/Join";
+import Contact from "./components/Contact section/Contact";
 
 export default function App() {
   return (
     <div className="App">
       <Hero />
       <Join />
+      <Contact />
     </div>
   );
 }
